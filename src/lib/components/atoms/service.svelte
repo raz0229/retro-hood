ad830a7c924c7bb029285fe6ad02e4745912b29d
raz0229 <raja.zafar229@gmail.com>
2024-10-05 18:54:24 +0500

    responsive services

--- src/lib/components/atoms/service.svelte
+++ src/lib/components/atoms/service.svelte
@@ -1,31 +1,88 @@
 <script>
-    export let serviceName = 'My Service'
+	export let serviceName = 'My Service';
+	export let details = 'lorem ipsum';
+
+	let isHovered = false;
 </script>
 
-<div class="service">
-    <div class="name text-4xl"> { serviceName } </div>
+<div
+	on:mouseover={() => (isHovered = true)}
+	on:mouseout={() => (isHovered = false)}
+	class="service"
+>
+	<div class:filled={isHovered} class="z-20 hover:filled name text-5xl relative">
+		{serviceName}
+	</div>
+	<div
+		class:opacity-full={isHovered}
+		class="opacity-0 z-10 absolute ct mt-[-5rem] ml-[-4rem] mr-8 flex"
+		style="gap: 2rem; transition: opacity 0.5s ease-in-out;"
+	>
+		<img src="hero.jpg" alt={serviceName} />
+		<div class="details">
+			<div class="text-3xl pt-2 pb-2 text-white">
+				{serviceName}
+			</div>
+			<div class="border-t-2 border-t-[#6d6d6d] pt-6 text-[#6d6d6d]">
+				<p class="text-xl" style="line-height: 1.3rem;">
+					{details}
+				</p>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
-    .service {
-        margin: 4rem 16rem;
+
+    img {
+        width: 26rem;
     }
 
-    .service .name {
-        -webkit-text-fill-color: transparent;
-        -webkit-text-stroke: 0.5px;
-        -webkit-text-stroke-color: #ffffff;
-    }
+	.service {
+		margin: 4rem 16rem;
+        cursor: pointer;
+	}
 
-    @media only screen and (max-width: 850px) {
-        .service {
-            margin: 3rem 12rem;
+	.filled {
+		-webkit-text-fill-color: crimson !important;
+		-webkit-text-stroke: 0 !important;
+	}
+
+	.opacity-full {
+		opacity: 100%;
+	}
+
+	.service .name {
+		-webkit-text-fill-color: transparent;
+		-webkit-text-stroke: 0.5px;
+		-webkit-text-stroke-color: #ffffff;
+		font-weight: 600;
+	}
+
+	@media only screen and (max-width: 950px) {
+		.service {
+			margin: 3rem 12rem;
+		}
+        img {
+            width: 20rem;
+            height: 12rem;
         }
-    }
-
-    @media only screen and (max-width: 600px) {
-        .service {
-            margin: 2rem 4rem;
+        .ct {
+            display: none;
         }
-    }
+	}
+
+	@media only screen and (max-width: 800px) {
+		.service {
+			margin: 2rem 4rem;
+		}
+        img {
+            width: 15rem;
+            height: 10rem;
+        }
+
+        .ct {
+            display: none;
+        }
+	}
 </style>
--- src/lib/components/services.svelte
+++ src/lib/components/services.svelte
@@ -4,25 +4,39 @@
 
     const services = [
         {
-            name: 'Manufacturing'
+            name: 'Manufacturing',
+            image: 'services/manufacturing.jpg',
+            details: 'From the start of your project, you\'ll have a dedicated Client Account Manager ensuring a smooth transition through every stage—initial concept, sampling, development, and ultimately production. Our expert teams - client managers, developers, and merchandising specialists - respond swiftly to your needs. We deliver innovative solutions through continuous research and testing, prioritizing sustainability.'
         },
         {
-            name: 'Procurement'
+            name: 'Procurement',
+            image: 'services/procurement.jpg',
+            details: 'Our extensive procurement division sources diverse fabrics globally, offering everything from basic single jersey to high-quality 500 gsm loopback cotton and terry fleece. We partner with suppliers across Pakistan to guarantee quality and environmental commitment, offering organic cotton, viscose, poly, and various blends.'
         },
         {
-            name: 'Printing'
+            name: 'Printing',
+            image: 'services/printing.jpg',
+            details: 'Our state-of-the-art printing factory employs various techniques, including heat-sensitive printing, heat transfer, and digital printing on advanced TAS machines. With cutting-edge equipment, we ensure a high-quality product with a monthly output of 10,000 – 13,000 panels.'
         },
         {
-            name: 'Treatment'
+            name: 'Treatment',
+            image: 'services/treatment.jpg',
+            details: 'Our in-house Tajima embroidery machines, stone application, heat transfers, and other specialist equipment ensure premium quality.We also collaborate with trusted vendors for unique finishes like tie-dyeing and marble washing, using eco-friendly, water-based inks.'
         },
         {
-            name: 'Sewing'
+            name: 'Sewing',
+            image: 'services/sewing.jpg',
+            details: 'With 10 dedicated full-time operators across seven lines, our daily output can reach 100 units. Our team, skilled in using 3-5 different machine types, ensures diverse, high-quality products with minimal downtime.'
         },
         {
-            name: 'Warehousing'
+            name: 'Warehousing',
+            image: 'services/warehousing.jpg',
+            details: 'We provide comprehensive storage and warehouse solutions, managing individual store packing, direct dispatch, and specialized processing for department stores. Our ERP system ensures transparency and efficiency in managing goods movement and storage.'
         },
         {
-            name: 'Quality Control'
+            name: 'Quality Control',
+            image: 'services/quality-control.jpg',
+            details: 'Adhering to international AQL 2.5 standards, we conduct AQL 1.5 final inspections and additional internal QC checkpoints at each production stage. Our testing facility ensures every garment meets durability, colorfastness, tear strength, weight, fabric twist, and shrinkage standards'
         }
     ]
 </script>
@@ -31,7 +45,10 @@
     <Tag text="Services"/>
 
     {#each services as service}
-        <Service serviceName="{service.name}"/>
+        <Service 
+            serviceName="{service.name}"
+            details="{service.details}"
+        />
     {/each}
 </div>
 
