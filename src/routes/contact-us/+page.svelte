b97b3ead9606eecc76e2c85987ee11d7fd0da7f1
raz0229 <raja.zafar229@gmail.com>
2024-10-09 14:38:30 +0500

    changed favicon

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,14 +1,40 @@
+<script>
+    import Tag from '$lib/components/atoms/tag.svelte';
+	import { scale } from 'svelte/transition';
+	
+	let animate = false;
+	function actionWhenInViewport(e) {
+		const observer = new IntersectionObserver((entries) => {
+			if (entries[0].isIntersecting) {
+				// element in viewport
+				animate = true;
+			} else {
+				animate = false;
+			}
+		});
+
+		observer.observe(e);
+	}
+</script>
+
 <svelte:head>
-    <title>RetroHood | Contact</title>
+	<title>RetroHood | Contact</title>
 </svelte:head>
 
-
-<div class="container sm:items-stretch p-16">
-    
+<div use:actionWhenInViewport class="container sm:items-stretch p-16">
+	<Tag text="Get in touch" />
+	{#if animate}
+		<h1
+			transition:scale={{ duration: 500, start: 0.5 }}
+			class="text-white font-extrabold text-5xl lg:pl-[12rem] md:pl-32 pl-8"
+			style="line-height: 2.75rem;"
+		>
+			Let us tell your story through visuals
+		</h1>
+	{/if}
 </div>
 
 <style>
-
 	.container,
 	.container-white {
 		margin: 0;
