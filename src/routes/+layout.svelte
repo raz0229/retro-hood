6b08c9e843cfca2fedb8b9caaf2e3d4cebd79c96
raz0229 <raja.zafar229@gmail.com>
2024-10-09 15:01:51 +0500

    added route: contact us

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,8 @@
     import { page } from "$app/stores"
   
     $: color = $page.url.pathname === '/process' ||
-              $page.url.pathname === '/about-us' 
+              $page.url.pathname === '/about-us' ||
+              $page.url.pathname === '/contact-us' 
                 ? '#0a0909'
                 : '#f8f8f8'
 
