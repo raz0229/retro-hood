fdf5acc690165c8227ff8808ae6651dddd9dea11
raz0229 <raja.zafar229@gmail.com>
2024-10-14 09:31:11 +0500

    get a quote button animation

--- src/lib/components/atoms/action-button.svelte
+++ src/lib/components/atoms/action-button.svelte
@@ -1,16 +1,39 @@
 <script>
     export let text = 'Get a quote'
+
+	let sourceImage = 'arrow-slanted.png'
+
+
+	const hoverHandler = () => {
+		console.log('Hovered ', text)
+		sourceImage = 'arrow-slanted-white.png'
+	}
+
+	const hoverLeave = () => {
+		sourceImage = 'arrow-slanted.png';
+	}
+
 </script>
-<div class="quote relative mt-12 ml-16">
+<div 
+	on:mouseenter={hoverHandler}
+	on:mouseleave={hoverLeave}
+	on:click={()=>location.href = '/contact-us'}
+	class="quote relative mt-12 ml-16">
     <div class="circle-arrow absolute">
         <div class="svg">
-            <img src="arrow-slanted.png" alt="" />
+            <img src="{sourceImage}" alt="" />
         </div>
     </div>
     <button>&nbsp;{text}&nbsp;</button>
 </div>
 
 <style>
+
+	.quote {
+		transition: all 0.3s ease;
+		cursor: pointer;
+	}
+
     	.quote button {
 		border-radius: 20px;
 		background-color: #cb0f0f;
@@ -21,8 +44,13 @@
 		transition: background-color 0.3s ease;
 	}
 
-	.quote button:hover {
-		background-color: #b51111;
+	.quote:hover {
+		transform: scale(1.15);
+	}
+
+	.quote:hover button {
+		color: #cb0f0f;
+		background-color: #fff;
 	}
 
 	.circle-arrow {
@@ -35,9 +63,20 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		transition: all 0.3s ease;
+	}
+
+	.quote:hover .circle-arrow {
+		border: solid 1px #ffffff;
+		background-color: #cb0f0f;
 	}
 
 	.circle-arrow > .svg img {
-		width: 1.3rem;
+		width: 1.6rem;
+		transition: transform 0.4s ease;
+	}
+
+	.quote:hover .circle-arrow > .svg img {
+		transform: translate(90%, -90%);
 	}
 </style>
