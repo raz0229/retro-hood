72fb864becfa63fb711055cd0e405eef5a2c40a8
raz0229 <raja.zafar229@gmail.com>
2024-10-17 10:41:05 +0500

    comment 75 - added process pictures

--- src/lib/components/atoms/item.svelte
+++ src/lib/components/atoms/item.svelte
@@ -10,8 +10,8 @@
 
 </script>
 
-<div class="w-72 mt-32">
-    <h1 class="text-[#6d6d6d] mt-4 mb-2 font-extralight text-2xl" style="line-height: 1.25rem;">
+<div class="w-{item.width ?? '72'} mt-32 p-{item.padding ?? '0'} bg-[{item.backgroundColor ?? 'transaprent'}]">
+    <h1 class="text-{item.indexColor ?? '[#6d6d6d]'} mt-4 mb-2 font-extralight text-2xl" style="line-height: 1.25rem;">
         {item.index}
     </h1>
     <h1 class="text-{item.titleColor ?? item.color} font-medium lg:text-3xl md:text-5xl text-3xl" style="line-height: 2.55rem;">
--- src/routes/process/+page.svelte
+++ src/routes/process/+page.svelte
@@ -23,36 +23,66 @@
 		{
 			index: '01',
 			title: 'Fabric Selection',
+			backgroundColor: '#cb0f0f',
+			indexColor: 'white',
+			padding: 16,
+			width: 100,
+			image: '/process/1.jpg',
 			details:
 				'Your dedicated representative will initially help you select the best fabric for your product.'
 		},
 		{
 			index: '02',
 			title: 'Cutting & Sewing',
+			backgroundColor: '#cb0f0f',
+			indexColor: 'white',
+			padding: 16,
+			width: 100,
+			image: '/process/2.jpg',
 			details:
 				'Once the fabric is selected, it is sent for Cut & Sew processes to make sure that the quality of the product is not compromised at all.'
 		},
 		{
 			index: '03',
 			title: 'Customization',
+			backgroundColor: '#cb0f0f',
+			indexColor: 'white',
+			padding: 16,
+			width: 100,
+			image: '/process/3.jpg',
 			details:
 				'At this step, all the changes you need in terms of printing, add-ons and changes are done effectively.'
 		},
 		{
 			index: '04',
 			title: 'Labelling',
+			backgroundColor: '#cb0f0f',
+			indexColor: 'white',
+			padding: 16,
+			width: 100,
+			image: '/process/4.jpg',
 			details:
 				'Once your product is finalized, we make sure that every piece has your label before it is sent to the packaging department.'
 		},
 		{
 			index: '05',
 			title: 'Packaging',
+			backgroundColor: '#cb0f0f',
+			indexColor: 'white',
+			padding: 16,
+			width: 100,
+			image: '/process/5.jpg',
 			details:
 				'The final product is ironed, folded with care and packed in the baggage of your choice to give it a premium look.'
 		},
 		{
 			index: '06',
 			title: 'Shipment',
+			backgroundColor: '#cb0f0f',
+			indexColor: 'white',
+			padding: 16,
+			width: 100,
+			image: '/process/6.jpg',
 			details:
 				'Once the order is packed and ready, it is sent for shipment to your location with minimum shipping time.'
 		}
@@ -119,18 +149,18 @@
 </div>
 
 <div 
-	class="container-white sm:items-stretch p-16">
+	class="container sm:items-stretch lg:p-16 md:p-8 p-2">
 	<Tag text="Process" />
-	<div class="flex justify-between lg:p-16">
+	<div class="flex justify-between lg:p-16 md:p-8 p-8">
 		<div>
 			<h1
-				class="text-black font-medium lg:text-5xl md:text-5xl text-3xl lg:pl-[12rem] md:pl-32 pl-2"
+				class="text-white font-medium lg:text-5xl md:text-5xl text-3xl lg:pl-[12rem] md:pl-32 pl-2"
 				style="line-height: 2.75rem;"
 			>
 				Quality is Everything
 			</h1>
 			<h1
-				class="text-black mt-4 font-light text-2xl lg:pl-[12rem] md:pl-32 pl-2"
+				class="text-white mt-4 font-light text-2xl lg:pl-[12rem] md:pl-32 pl-2"
 				style="line-height: 1.25rem;"
 			>
 				May it be about yourself or others, We <br />
@@ -140,24 +170,33 @@
 		</div>
 		<div
 			style="max-width: 8rem;"
-			class="lg:block md:hidden hidden image lg:p-12 md:p-8 p-2 lg:mr-[12rem] md:mr-32 mr-2 bg-black rounded-full"
+			class="lg:block md:hidden hidden image lg:p-12 md:p-8 p-2 lg:mr-[12rem] md:mr-32 mr-2 bg-red-700 rounded-full"
 		>
 			<img style="width: 2rem; rotate: -180deg; " src="arrow-slanted-white.png" alt="" />
 		</div>
 	</div>
 
-	<div
-		class="lg:grid lg:pl-42 lg:pr-42 md:flex md:flex-wrap block lg:grid-cols-12 lg:grid-rows-12 gap-4"
-	>
+
+	{#each processes as process, i}
+	<div style="align-items: end;" class="flex lg:flex-row md:flex-col flex-col justify-between lg:gap-24 md:gap-16 gap-4 lg:mr-64 lg:ml-64 md:mr-16 md:ml-16 mr-4 ml-4">
+		{#if i%2==0}
+		<div class="flex-1"><Item item={process} /></div>
+		<div class="image flex-1">
+			<img style="width: 40rem;" src="{process.image}" alt="/hero.jpg">
+		</div>
+		{:else}
+		<div class="image flex-1">
+			<img style="width: 40rem;" src="{process.image}" alt="/hero.jpg">
+		</div>
+		<div class="flex-1"><Item item={process} /></div>
 		
-		<div class="col-span-3 row-span-3 col-start-3"><Item item={processes[0]} /></div>
-		<div class="col-span-3 row-span-3 col-start-8 row-start-2"><Item item={processes[1]} /></div>
-		<div class="col-span-3 row-span-3 col-start-3 row-start-5"><Item item={processes[2]} /></div>
-		<div class="col-span-3 row-span-3 col-start-8 row-start-6"><Item item={processes[3]} /></div>
-		<div class="col-span-3 row-span-3 col-start-3 row-start-9"><Item item={processes[4]} /></div>
-		<div class="col-span-3 row-span-3 col-start-8 row-start-10"><Item item={processes[5]} /></div>
-		
+		{/if}
 	</div>
+	{/each}
+	
+		
+
+	
 </div>
 
 <div
